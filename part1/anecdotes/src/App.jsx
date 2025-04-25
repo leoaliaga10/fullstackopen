b2370f8c7798c51ef745e247cda4c133b67441e0
Leoncio Aliaga Saldana <leo.as10@gmail.com>
2025-04-25 10:16:27 -0500

    Commit of exercise 1.12

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const App = () => {
+  let number_randon;
+  const getRandonInt = (max) => Math.floor(Math.random() * max);
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  //console.log(anecdotes.length);
+
+  const [selected, setSelected] = useState(0);
+
+  const handleClick = () => {
+    number_randon = getRandonInt(anecdotes.length);
+    console.log(number_randon);
+    setSelected(number_randon);
+  };
+
+  return (
+    <>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={handleClick} text="next anecdote" />
+    </>
+  );
+};
+
+export default App;

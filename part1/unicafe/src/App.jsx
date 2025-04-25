48db280a83b1886896f9bdeb8b30d0808c207d1d
Leoncio Aliaga Saldana <leo.as10@gmail.com>
2025-04-25 00:13:04 -0500

    Commit of exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -27,22 +27,41 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [sum, setSum] = useState(0);
+  const [avg, setAvg] = useState(0);
+  const [pos, setPos] = useState(0);
 
   const handleGoodClick = () => {
     const updatedGood = good + 1;
+    const updatedSum = updatedGood + neutral + bad;
+    const updatedAvg = (updatedGood * 1 + bad * -1) / updatedSum;
+    const updatedPos = (updatedGood / updatedSum) * 100;
     setGood(updatedGood);
-    // setTotal(updatedLeft + right);
+    setSum(updatedSum);
+    setAvg(updatedAvg);
+    setPos(updatedPos);
   };
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1;
+    const updatedSum = updatedNeutral + good + bad;
+    const updatedAvg = (good * 1 + bad * -1) / updatedSum;
+    const updatedPos = (good / updatedSum) * 100;
     setNeutral(updatedNeutral);
-    // setTotal(updatedNeutral + left);
+    setSum(updatedSum);
+    setAvg(updatedAvg);
+    setPos(updatedPos);
   };
 
   const handleBadClick = () => {
     const updatedBad = bad + 1;
+    const updatedSum = updatedBad + good + neutral;
+    const updatedAvg = (updatedBad * -1 + good * 1) / updatedSum;
+    const updatedPos = (good / updatedSum) * 100;
     setBad(updatedBad);
+    setSum(updatedSum);
+    setAvg(updatedAvg);
+    setPos(updatedPos);
   };
 
   return (
@@ -55,6 +74,9 @@ const App = () => {
       <Par text="good" total={good} />
       <Par text="neutral" total={neutral} />
       <Par text="bad" total={bad} />
+      <Par text="all" total={sum} />
+      <Par text="average" total={avg} />
+      <Par text="positive" total={pos} />
     </>
   );
 };

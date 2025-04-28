933406fee254cae20f7add91d8453edfa5873fa6
Leoncio Aliaga Saldana <leo.as10@gmail.com>
2025-04-28 08:33:22 -0500

    Commit of exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-1234567" },
+    { name: "Arto Hellas", phone: "040-123456", id: 1 },
+    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [textFind, setTextFind] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -13,7 +17,6 @@ const App = () => {
       name: newName,
       phone: newPhone,
     };
-    console.log(nameObject);
 
     //..................
     let pivot = false;
@@ -37,6 +40,18 @@ const App = () => {
     setNewPhone(event.target.value);
   };
 
+  const handleFindChange = (event) => {
+    setTextFind(event.target.value);
+  };
+
+  function filtrarPorNombre(text) {
+    const textoEnMinusculas = text.toLowerCase();
+    return persons.filter((person) =>
+      person.name.toLowerCase().includes(textoEnMinusculas)
+    );
+  }
+  const resultadoFiltro = filtrarPorNombre(textFind);
+
   const Persons = ({ names, phones }) => {
     return (
       <li>
@@ -48,6 +63,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={textFind} onChange={handleFindChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -61,7 +81,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((persona, index) => (
+        {resultadoFiltro.map((persona, index) => (
           <Persons key={index} names={persona.name} phones={persona.phone} />
         ))}
       </ul>
